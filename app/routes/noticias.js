88e526e091d9998e42e394cef78298edea9be32d
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-06-14 15:32:20 -0400

    aula 14-06-24 v2

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -2,7 +2,7 @@
 
 module.exports = function (app) {
     app.get("/noticias", function (req, res) {
-        s
+        
         var conexao = app.config.dbConnection();
         conexao.query('select * from noticias', function (error, result) {
             res.render('noticias/noticias', { noticias: result })
