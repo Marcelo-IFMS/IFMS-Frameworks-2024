f1b128ab3a1d8b9f993277a55129de5153906b64
Marcelo Tomporoski Perez <marcelotperez@gmail.com>
2024-06-20 10:48:37 -0400

    BackGroundColor-form post

--- app/routes/form_add_news.js
+++ app/routes/form_add_news.js
@@ -2,4 +2,9 @@ module.exports=function(app){
     app.get("/formulario_inclusao_noticias", function(req, res){
         res.render("admin/form_add_news");
     });
+    app.post('/formulario_inclusao_noticias', function (req, res) {
+        console.log(req.body);
+        //res.send("ola");
+        res.render('admin/form_add_news', { cadok: 1 })
+    }) 
 }
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,21 +1,17 @@
 module.exports = function (app) {
     app.get("/noticias", function (req, res) {
-               var conexao = app.config.dbConnection();
+        var conexao = app.config.dbConnection();
         conexao.query('select * from noticias', function (error, result) {
             res.render('noticias/noticias', { noticias: result })
         });
     })
     app.get("/noticias/:id", function (req, res) {
         res.send("ola");
-       
-    })   
+
+    })
     app.get("/noticias/:Identificacao/:NomeCompleto", function (req, res) {
         res.send("ola");
-       
-    })   
-    app.post('/noticias', function (req, res) {
-        console.log(req.body);
-        res.send("ola");
-       
-    })   
+
+    })
+
 }
