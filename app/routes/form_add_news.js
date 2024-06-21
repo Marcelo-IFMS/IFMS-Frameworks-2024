d8938638f8aae15bcc5659cee563c7272f480465
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-06-21 08:54:27 -0400

    erro em form add news

--- app/routes/form_add_news.js
+++ app/routes/form_add_news.js
@@ -5,8 +5,7 @@ module.exports = function (app) {
     app.post('/formulario_inclusao_noticias', function (req, res) {
         var conexao = app.config.dbConnection();
         conexao.query("INSERT INTO noticias (titulo,noticia) VALUES ('" + req.body.titulo + "','" + req.body.noticia + "')", function (error, result) {
-            if (error != null){
-
+            if (error != null) {
                 res.render('secao/404');
             } else {
                 res.render('admin/form_add_news', { cadok: 1 })
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -6,20 +6,21 @@ module.exports = function (app) {
         });
     })
     app.get("/noticias/:id", function (req, res) {
-        res.send("ola");
+        res.send("ola id");
 
     })
     app.get("/noticias/:Identificacao/:NomeCompleto", function (req, res) {
-        res.send("ola");
+        res.send("ola identificação e nome completo");
 
     })
-    app.post("/noticias/:Identificacao/:NomeCompleto", function (req, res) {
-        // var conexao = app.config.dbConnection();
+    app.post("/noticias/:titulo/:noticia", function (req, res) {
+        var conexao = app.config.dbConnection();
         var queryNoticias 
         = "INSERT INTO noticias (titulo,noticia) VALUES ('"
-        +req.params.Identificacao+"','"+req.params.NomeCompleto+"')"; 
+        +req.params.titulo+"','"+req.params.noticia+"')"; 
         console.log(queryNoticias);
-        res.send("olá");
+        conexao.query(queryNoticias,function (error, result) {
+            res.send("Gravou no Banco?");
+        })
     })
-
 }
