d61a51c066e77561bc06519a48b209279ab4fb90
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-06-23 17:39:52 -0400

    Menu NavBar acoplado

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,4 +2,7 @@ module.exports=function(app){
     app.get("/", function(req, res){
         res.render("home/index");
     });
+    app.get("/index", function(req, res){
+        res.render("index");
+    });
 }
