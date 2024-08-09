5f021f79c031210cb451129d845afb52437a42bf
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-08-09 10:34:22 -0400

    Aula 08/08

--- app/routes/form_add_news.js
+++ app/routes/form_add_news.js
@@ -1,15 +1,44 @@
 module.exports = function (app) {
     app.get("/formulario_inclusao_noticias", function (req, res) {
-        res.render("admin/form_add_news");
-    });
-    app.post('/formulario_inclusao_noticias', function (req, res) {
         var conexao = app.config.dbConnection();
-        conexao.query("INSERT INTO noticias (titulo,noticia) VALUES ('" + req.body.titulo + "','" + req.body.noticia + "')", function (error, result) {
+        conexao.query('select * from tiponoticia', function (error, result) {
+            console.log(result);
+            res.render('admin/form_add_news', { noticias: result })
+        });
+        //res.render("admin/form_add_news");
+    });
+    app.post("/formulario_inclusao_noticias", function (req, res) {
+        var cadOk=true;
+        console.log(req.body);
+        res.render("admin/form_add_news",{cadOk});
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // res.send("OK o título é:"+req.body.titulo);
+        /* insert banco de dados */
+    // app.post('/formulario_inclusao_noticias', function (req, res) {
+    //     console.log(req.body);
+    //     res.send("ola");
+        
+        //var conexao = app.config.dbConnection();
+       /*  conexao.query("INSERT INTO noticias (titulo,noticia) VALUES ('" + req.body.titulo + "','" + req.body.noticia + "')", function (error, result) {
             if (error != null) {
                 res.render('secao/404');
             } else {
                 res.render('admin/form_add_news', { cadok: 1 })
             }
-        })
-    })
-}
+        }) */
+    // })
--- config/server.js
+++ config/server.js
@@ -1,17 +1,15 @@
 var consign = require("consign"); //carrega o consign que automatiza as rotas **npm install --save consign**
 var express = require("express");//carrega o expresse que implementa as rotas **npm install --save express**
 var bodyParser = require('body-parser')// diponibiliza a utilização de variavéis passadas por post em form´s **npm install --save body-parser**
-
 // implemente o express na variável  app
 var app = express();
 //identificar a engine e pasta onde estão as views
 app.set("view engine", "ejs");
- app.set("views", "./app/views");
-
+app.set("views", "./app/views");
 //Utilizado para disponibilizar acesso aos arquivos CSS e JS 
 app.use(express.static("./app/public"));
 //configura em app a utilização das variávies passadas por post de form´s 
-app.use( bodyParser.json() );  
+app.use(bodyParser.json() );  
 app.use(bodyParser.urlencoded({
   extended: true
 }));
